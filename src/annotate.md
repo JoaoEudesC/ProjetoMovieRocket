7a2eecc6b325af2a613da7e17e364faed14c023a
joaoeudes2501 <joaoeudes2501200@gmail.com>
2023-03-13 15:26:18 +0000

    Finalização de componetização

---
+++ src/annotate.md
@@ -0,0 +1,21 @@
+## **\*\***\*\*\*\***\*\***\*\***\*\***\*\*\*\***\*\*** => Praticas e dicas sobre componetização no react
+
+## 1 - Executando a aplicação no yarn => yarn install (Intala as dependencias) é igual quando voce clona um projeto npm e voce tem que executar o comando npm install para instalar as dependencias do projeto
+
+## ********\*\*********\*\*\*\*********\*\********* => quando eu clonar um repositório da rocketSeat eu tenho que executar estes comandos para que o projeto funcione com o yarn e o sass
+
+## 2 -Se você estiver usando o Yarn, pode tentar as seguintes soluções:
+
+Verifique se a versão do Node.js está atualizada para a versão mais recente. Você pode verificar a versão do Node.js executando o comando "node -v" no terminal.
+
+Tente instalar a versão mais recente do Node Sass executando o seguinte comando no terminal: "yarn add node-sass@latest".
+
+Se você estiver usando o Yarn 2, pode tentar usar o comando "yarn set version berry" para atualizar para a versão mais recente do Yarn. Em seguida, execute "yarn install" para instalar as dependências do projeto novamente.
+
+Se nenhuma das soluções acima funcionar, tente atualizar as dependências do projeto para as versões mais recentes. Você pode fazer isso executando o comando "yarn upgrade".
+
+Se nada disso funcionar, verifique se o seu ambiente Linux é suportado pelo Node Sass. Você pode encontrar a lista de ambientes suportados no link indicado na mensagem de erro.
+
+Espero que isso ajude a resolver o problema!
+
+## 4 - Repare que neste exercicio eu fiz a utilização de componentes para o meu codigo ficar mais bem aproveitado e organizado , diminui muitos arquivos no app.tsx
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,75 @@
-export function Content() {
-  // Complete aqui
+//Importação de hooks
+import { useState, useEffect } from "react";
+
+//Importação de componentes
+import { MovieCard } from "./MovieCard";
+
+//Importação de api
+import { api } from "../services/api";
+
+//Criaçaõ de interfaces
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface PropGenreId {
+  selectGenreId: number;
+}
+
+export function Content(props: PropGenreId) {
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${props.selectGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+
+    api
+      .get<GenreResponseProps>(`genres/${props.selectGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [props.selectGenreId]);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
